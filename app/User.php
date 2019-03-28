4dcc6314354115f5dc7426cb6f4486bf1da4a883
unknown <mindaugaszinkevicius1@gmail.com>
2019-03-28 21:49:55 +0200

    Basic Login

--- app/User.php
+++ app/User.php
@@ -36,4 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    const TYPE_ADMIN = 0;
+    const TYPE_USER = 1;
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('user_type')->default(\App\User::TYPE_USER);
             $table->rememberToken();
             $table->timestamps();
         });
